59903e0ebc45ee7a7c12eb171a8caa515d755b75
Ranidhumma <ranidhumma8@gmail.com>
2022-03-20 20:43:12 +0530

    Welcome

---
+++ src/com/java/BinarySearch.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to Binary Serach");
+	}
+}
